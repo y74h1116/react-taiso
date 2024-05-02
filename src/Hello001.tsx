09d9208c93a3f9bf38ba39a005b42f29496bb291
y74h1116 <local@y74h1116.com>
2024-05-02 15:35:40 +0900

    Hello001 に枠をつける

--- src/Hello001.tsx
+++ src/Hello001.tsx
@@ -1,7 +1,10 @@
 import { FC } from "react";
 
 export const Hello001:FC = () => {
+    const style = {border: 'solid'};
     return (
-        <p>hello 001</p>
+        <div style={style}>
+            <p>hello 001</p>
+        </div>
     );
 };
