900204b0f5d9d282d1f005436cbfe84e87311ce3
y74h1116 <local@y74h1116.com>
2024-05-02 15:58:11 +0900

    useState を利用して、useReducer も使ってみる

--- src/Hello001Text.tsx
+++ src/Hello001Text.tsx
@@ -1,13 +1,22 @@
-import { FC, useState } from "react";
+import { FC, useReducer, useState } from "react";
 
 export const Hello001Text:FC = () => {
     const [strInput, setStrInput] = useState('');
 
+    const reducerFunc = (state:string, action:string) => {
+        return state + ' => ' + action;
+    };
+    const [reducerState, dispatchFunc] = useReducer(reducerFunc, strInput);
+
     const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
         setStrInput(event.target.value);
+        dispatchFunc(event.target.value);
     };
 
     return (
-        <input type="text" value={strInput} onChange={onChangeHandler}/>
+        <div>
+            <input type="text" value={strInput} onChange={onChangeHandler}/>
+            <p>{reducerState}</p>
+        </div>
     );
 };
