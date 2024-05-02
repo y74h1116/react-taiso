a535164bc0f5b254be719f11ab34be702d17f70f
y74h1116 <local@y74h1116.com>
2024-05-02 15:34:19 +0900

    Hello001 を App の jsx の一番上の方に追加し、ブラウザで表示を確認する

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Hello001 } from './Hello001'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Hello001/>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
